e536b7814f867aa6017f79eecdc94e497dd02c52
Denis <denis2000.kuzik@gmail.com>
2023-10-07 21:23:49 +0300

    wip

---
+++ utils/env_tuner.py
@@ -0,0 +1,32 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+
+def get_tuned_env():
+    env = gym_super_mario_bros.make('SuperMarioBros-v3')
+    
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    return env
+
+
+def max_and_skip_env(env):
+    """Every action the agent makes is repeated over 4 frames"""
+    return env
+
+def process_frame84(env):
+    """The size of each frame is reduced to 84×84"""
+    return env
+
+def image_to_pytorch(env):
+    """The frames are converted to PyTorch tensors"""
+    return env
+
+def buffer_wrapper(env):
+    """Only every fourth frame is collected by the buffer"""
+    return env
+
+def scaled_float_frame(env):
+    """The frames are normalized so that pixel values are between 0 and 1"""
+    return env
+
