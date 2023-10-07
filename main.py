cd8fe3bff1037ede802092cf8820b7592c6862c4
Denis <denis2000.kuzik@gmail.com>
2023-10-07 22:26:36 +0300

    wip

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import utils.env_tuner as env_tuner
 
 if __name__ == '__main__':
     env = env_tuner.get_tuned_env()
+    print(env.observation_space.sample)
---
+++ utils/wrappers/env_wrappers.py
@@ -0,0 +1,7 @@
+import gym
+
+class FrameResizer(gym.ObservationWrapper):
+    def __init__(self, env):
+        pass
+        super().__init__(env)
+        #self.observation_space = 
